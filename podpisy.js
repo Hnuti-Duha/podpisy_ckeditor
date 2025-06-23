5fc5ccb06c0e425e23bdbfb4b3fa57fdaec53e89
Miloš Kroulík <milos.kroulik@gmail.com>
2025-06-23 11:07:00 +0200

    Update podpisy.js

--- podpisy.js
+++ podpisy.js
@@ -14,37 +14,37 @@ CKEDITOR.addTemplates( 'default', {
 		title: 'Další úspěch',
 		image: 'template1.gif',
 		description: 'Další úspěch může být náš společný. Staňte se dárcem či dárkyní Hnutí DUHA na darce.hnutiduha.cz.',
-		html: 'Další úspěch může být náš společný. Staňte se dárcem či dárkyní Hnutí DUHA na <a href="https://darce.hnutiduha.cz/#widget">darce.hnutiduha.cz</a>.'
+		html: 'Další úspěch může být náš společný. Staňte se dárcem či dárkyní Hnutí DUHA na <a style="color: #009540;font-weight: 600;" href="https://darce.hnutiduha.cz/#widget">darce.hnutiduha.cz</a>.'
 	},
     {
 		title: 'Příští úspěch',
 		image: 'template1.gif',
 		description: 'Náš příští úspěch může být společný! Staňte se i Vy přáteli Hnutí DUHA. Více na darce.hnutiduha.cz.',
-		html: 'Náš příští úspěch může být společný! Staňte se i Vy přáteli Hnutí DUHA<br>Více na <a href="http://darce.hnutiduha.cz/">darce.hnutiduha.cz</a>.'
+		html: 'Náš příští úspěch může být společný! Staňte se i Vy přáteli Hnutí DUHA. Více na <a style="color: #009540;" href="http://darce.hnutiduha.cz/">darce.hnutiduha.cz</a>.'
 	},
     {
 		title: 'I Vy můžete',
 		image: 'template1.gif',
 		description: 'I Vy můžete pomoci přírodě. Podpořte Hnutí DUHA na darce.hnutiduha.cz.',
-		html: 'I Vy můžete pomoci přírodě. Podpořte Hnutí DUHA na <a href="https://darce.hnutiduha.cz">darce.hnutiduha.cz</a>.'
+		html: 'I Vy můžete pomoci přírodě. Podpořte Hnutí DUHA na <a style="color: #009540;" href="https://darce.hnutiduha.cz">darce.hnutiduha.cz</a>.'
 	},
     {
 		title: 'Podpořte Hnutí DUHA',
 		image: 'template1.gif',
 		description: 'Podpořte Hnutí DUHA na darce.hnutiduha.cz.',
-		html: 'Podpořte Hnutí DUHA na <a href="http://darce.hnutiduha.cz/">darce.hnutiduha.cz</a>.'
+		html: 'Podpořte Hnutí DUHA na <a style="color: #009540;" href="http://darce.hnutiduha.cz/">darce.hnutiduha.cz</a>.'
 	},
     {
 		title: 'Staňte se i vy',
 		image: 'template1.gif',
 		description: 'Staňte se i Vy přáteli Hnutí DUHA. Bez pomoci lidí se naše práce neobejde. Více na http://darce.hnutiduha.cz.',
-		html: 'Staňte se i Vy přáteli Hnutí DUHA. Bez pomoci lidí se naše práce neobejde. Více na <a href="http://darce.hnutiduha.cz/">http://darce.hnutiduha.cz</a>.'
+		html: 'Staňte se i Vy přáteli Hnutí DUHA. Bez pomoci lidí se naše práce neobejde. Více na <a style="color: #009540;" href="http://darce.hnutiduha.cz/">http://darce.hnutiduha.cz</a>.'
 	},
     {
 		title: 'I Vy můžete pomoci přírodě',
 		image: 'template1.gif',
 		description: 'I Vy můžete pomoci přírodě. Podpořte Hnutí DUHA na darce.hnutiduha.cz nebo dejte své srdce planetě ve Vaší závěti.',
-		html: 'I Vy můžete pomoci přírodě. Podpořte Hnutí DUHA na <a href="https://darce.hnutiduha.cz/">darce.hnutiduha.cz</a> nebo dejte své srdce planetě ve Vaší závěti.'
+		html: 'I Vy můžete pomoci přírodě. Podpořte Hnutí DUHA na <a style="color: #009540;" href="https://darce.hnutiduha.cz/">darce.hnutiduha.cz</a> nebo dejte své srdce planetě ve Vaší závěti.'
 	}			
  ]
 } );
